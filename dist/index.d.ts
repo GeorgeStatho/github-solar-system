1b4860942008432cb4a868738145a58b448ff6bf
Huvle <125417269+GeorgeStatho@users.noreply.github.com>
2026-03-22 16:16:40 -0400

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { S as SolarSystemData, a as SolarSystem } from './SolarSystem-DppH1h7S.js';
-export { B as BranchData, R as RepoData, U as UserData } from './SolarSystem-DppH1h7S.js';
-
-declare function createSolarSystemData(data: SolarSystemData): SolarSystem;
-
-export { SolarSystemData, createSolarSystemData };
--- dist/react.d.ts
+++
@@ -1,8 +0,0 @@
-import * as react_jsx_runtime from 'react/jsx-runtime';
-import { a as SolarSystem } from './SolarSystem-DppH1h7S.js';
-
-declare function RenderSolarSystem({ system }: {
-    system: SolarSystem;
-}): react_jsx_runtime.JSX.Element;
-
-export { RenderSolarSystem };
